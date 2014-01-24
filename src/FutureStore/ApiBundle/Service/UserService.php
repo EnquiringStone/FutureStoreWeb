aab7eb992cc6ed1cd44c0593e143b2957da3a7bd
EnquiringStone <johan-langes@hotmail.com>
2014-01-24 15:20:20 +0100

    Added api functions

--- src/FutureStore/ApiBundle/Service/UserService.php
+++ src/FutureStore/ApiBundle/Service/UserService.php
@@ -44,16 +44,44 @@ class UserService implements ApiInterface{
 		throw new \Exception('De gebruiker bestaat niet');
 	}
 
+	public function getProductByCode($data) {
+		if($user = $this->manager->getRepository('FutureStoreUserBundle:User')->findOneBy(array('login_token' => $data['login_token']))) {
+			$product = $this->manager->getRepository('FutureStoreSiteBundle:Product')->findOneBy(array('barcode' => $data['barcode']));
+			if(empty($product)) throw new \Exception('Product not found');
+			return array('name' => $product->getName(), 'price' => $product->getPrice());
+		}
+		throw new \Exception('Access denied');
+	}
+
+	public function getShoppingLists($data) {
+		if($user = $this->manager->getRepository('FutureStoreUserBundle:User')->findOneBy(array('login_token' => $data['login_token']))) {
+			$lists = array();
+			foreach($user->getShoppingLists() as $shoppingList) {
+				foreach($shoppingList->getShoppingListProducts() as $product) {
+					$lists[] = array(
+						'list_name' => $shoppingList->getListName(),
+						'product_name' => $product->getProduct()->getName(),
+						'product_price'	=> $product->getProduct()->getPrice(),
+						'quantity' => $product->getAmount()
+					);
+				}
+			}
+			if(empty($lists)) throw new \Exception('Geen lijst gevonden voor gebruiker '.$user->getUsername());
+			return array('shopping_lists' => $lists);
+		}
+		throw new \Exception('Access denied');
+	}
+
 	public function getName() {
 		return 'user';
 	}
 
 	/**
-	 * @param User $user
+	 * @param $user
 	 * @param $password
 	 * @return bool
 	 */
-	protected function validatePassword(User $user, $password) {
+	protected function validatePassword($user, $password) {
 		$encoder = $this->encoderFactory->getEncoder($user);
 		return $encoder->isPasswordValid($user->getPassword(), $password, $user->getSalt());
 	}
